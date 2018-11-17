469b017488ca8ac7bb2b0bfbcf43f82181b5fa97
Deepak Tyagi <depaktyagi1096@gmail.com>
2018-11-17 13:08:03 +0530

    final commit version 1.0

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,7 +4,7 @@ class Titles extends React.Component {
 	render(){
 		return(
 			<div>
-				<h1>Weather Finder</h1>
+				<h1 className="title-container__title">Weather Finder</h1>
 				<p>Find out temperature, conditions and more...</p>
 			</div>
 		);
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,11 +3,32 @@ import React from 'react';
 class Weather extends React.Component{
 	render(){
 		return(
-			<div>
-				<p>Location: {this.props.city}, {this.props.country}</p>
-				<p>Temperature: {this.props.temperature}</p>
-				<p>Humidity: {this.props.humidity}</p>
-				<p>Conditions: {this.props.description}</p>
+			<div clasName="weather__info">
+				{
+					this.props.city && this.props.country && <p className="weather__key">Location: 
+					<span className="weather__value"> {this.props.city}, {this.props.country}</span>
+					</p>
+				}
+				{
+					this.props.temperature && <p className="weather__key">Temperature: 
+					<span className="weather__value"> {this.props.temperature}</span>
+					</p>
+				}
+				{
+					this.props.humidity &&<p className="weather__key">Humidity: 
+					<span className="weather__value"> {this.props.humidity}</span>
+					</p>
+				}
+				{
+					this.props.description &&<p className="weather__key">Conditions: 
+					<span className="weather__value"> {this.props.description}</span>
+					</p>
+				}
+				{
+					this.props.error &&<p className="weather__key">
+					<span className="weather__value"> {this.props.error}</span>
+					</p>
+				}
 			</div>
 		);
 	}
